2fb2e5b0df69d1bed222bcef15d4cfe592334ec2
Elizabeth Dobryanskaya <dobrl2011@mail.ru>
2020-03-13 00:16:31 +0300

    FRONT-1: Создать фронтенд-репозиторий

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# donat-front
+# donut-front
 Frontend repo for project Donating
+### Доступ
+- Деплой (To be continued...)
+- [Бэкенд](https://github.com/BADgers-Team/donut-back) 
+- API (To be continued...)
+- Макеты (To be continued...)
+- [RoadMap](https://miro.com/welcomeonboard/aOeOpUF6JGmCrk0bvfZtKc2rBcYXIAlqgJ4zSUyksQ3ahCqhdZwt72D6hY539utF)
+- [UserFlow](https://app.flowmapp.com/projects/119316/userflow/75183/)
+- Опрос и результаты (To be continued...)
+### Установка и запуск  
+- `npm i`
+- `npm start` запуск проекта в режиме разработки
+- `npm run build` запуск продакшен-версии (статика)
+- `npm run linter`/`npm run linter-fix` запуск линтера
+### Структура проекта  
+- `src` - все ресурсы для разработки
+- `src/components` - компоненты, используемые в построении дерева
+- `src/components/app` - главный компонент-контейнер
+- `src/components/layouts` - контейнеры-страницы
+- `src/components/blocks` - компоненты-блоки, из которых формируются страницы (пр. навбар, таблица, футтер)
+- `src/components/fragments` - элементарные компоненты (пр. кнопки, селект-листы, чекбоксы)
+- `src/services` - все вспомогательные модули (пр. модуль для похода в сеть, хелперы)
+- `src/store` - константы
+### Дополнительно
+Есть файл .nvmrc, в котором записана текущая версия Node.js, с которой работает проект. Для корректной работы с этим файлом (если вам нужно) надо установить nvm, а затем `nvm use <указанная версия>`. Проверить корректность установки можно с помощью команды `nvm current`
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const src = path.join(__dirname, 'src');
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.[hash:8].js',
+        publicPath: '/',
+    },
+    resolve: {
+        alias: {
+            'components': path.join(src, 'components'),
+        },
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: path.join(__dirname, '/dist/index.html'),
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        publicPath: '/',
+        contentBase: '/',
+        hot: true,
+        historyApiFallback: true
+    },
+    optimization: {
+        minimize: true,
+    }
+};
