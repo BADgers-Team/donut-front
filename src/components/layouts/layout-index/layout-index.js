48adb2a0f9385bfb2f310cb5638e7dc2e15135f0
Elizabeth Dobryanskaya <dobrl2011@mail.ru>
2020-03-25 00:02:36 +0300

    Updated imports im styles

--- src/components/layouts/layout-index/layout-index.js
+++ src/components/layouts/layout-index/layout-index.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import BlockActivities from 'components/blocks/block-activities/block-activities';
 import BlockCards from 'components/blocks/block-cards/block-cards';
 
-import './layout-index.scss';
-
 class LayoutIndex extends Component {
     render() {
         return (
