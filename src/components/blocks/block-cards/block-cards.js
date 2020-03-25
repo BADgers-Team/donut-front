7f3230b7e4fb38aa4f629185444b9b8a68f5996e
Elizabeth Dobryanskaya <dobrl2011@mail.ru>
2020-03-25 03:22:35 +0300

    fix url to card img

--- src/components/blocks/block-cards/block-cards.js
+++ src/components/blocks/block-cards/block-cards.js
@@ -5,7 +5,7 @@ import SubscribersIcon from 'assets/img/subscribers.svg';
 import CardImage from 'assets/img/card-image.jpg';
 import Avatar from 'assets/img/michael.jpg';
 
-const backendUrl = 'http://localhost:8080';
+const backendUrl = 'http://localhost:8081';
 
 class BlockCards extends Component {
     render() {
@@ -27,7 +27,7 @@ export default BlockCards;
 class Card extends Component {
     render() {
         const { card } = this.props;
-        const cardPreview = card.files ? `${backendUrl}${card.files[0]}` : CardImage;
+        const cardPreview = card.files ? `${backendUrl}/${card.files[0]}` : CardImage;
         const cardAuthorAvatar = card.author.avatar ? card.author.avatar : Avatar;
         const cardSubscribers = card.subscribers ? card.subscribers : 120;
 
