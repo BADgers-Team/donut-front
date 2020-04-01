7926683ff9d80516758bcdd459f946e2b7134b3a
Erzhebet <dobrl2011@mail.ru>
2020-04-01 17:59:50 +0300

    Added change privacy type in profile

--- src/components/blocks/block-author-posts/block-author-posts.js
+++ src/components/blocks/block-author-posts/block-author-posts.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import { BlockPost } from 'components/blocks/block-author-posts/block-post/block-post';
 import Select from 'components/fragments/select/select';
 import { PRIVACY } from 'store/const';
+import RouteStore from 'store/routes';
+import AjaxModule from 'services/ajax';
+import { getUrlWithParams } from 'services/getUrlWithParams';
 
 import './block-author-posts.scss';
 import Avatar from 'assets/img/michael.jpg';
 
 class BlockAuthorPosts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+        };
+    }
+
+    componentDidMount() {
+        const { user } = this.props;
+        this.setState({ posts: user.posts });
+    }
+
     handleSelectChange = (event) => {
+        const { user } = this.props;
         const selectedPrivacy = event.target[event.target.selectedIndex].value;
-        console.log(selectedPrivacy);
+        AjaxModule.get(RouteStore.api.search, {
+            login: user.login,
+            visible_type: selectedPrivacy,
+            data_type: 'posts'
+        }).then((data) => {
+            this.setState({ posts: data.posts });
+        }).catch((error) => {
+            console.error(error.message);
+        });
     };
 
     render() {
         const { user } = this.props;
-        const postsNodes = user.posts && user.posts.length !== 0 ? user.posts.map((post) => {
+        const { posts } = this.state;
+        const postsNodes = posts && posts.length !== 0 ? posts.map((post) => {
             return <BlockPost key={post.id} post={post} login={user.login} avatar={user.avatar || Avatar}/>;
         }) : null;
 
@@ -35,9 +60,10 @@ class BlockAuthorPosts extends Component {
                     values={privacySelect}
                     actionType={Select.events.change}
                     onAction={this.handleSelectChange}
+                    classValue='author-posts__selector'
                 />
                 <div className="author-posts__body">
-                    {postsNodes}
+                    {postsNodes ? postsNodes : <div>Посты не найдены</div>}
                 </div>
             </div>
         );
