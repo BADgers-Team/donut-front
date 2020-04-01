a085be9d13440b21b7f80ddd0393a6fc554db2a4
kate <kirilek@bk.ru>
2020-04-01 18:31:31 +0300

    aaaaa

--- src/components/blocks/block-post-static/block-post-static.js
+++ src/components/blocks/block-post-static/block-post-static.js
@@ -68,7 +68,7 @@ class BlockPostStatic extends Component {
                         {post.visible_type === PRIVACY.OPEN && (
                             <div className="post-static__controls">
                                 <div className="post-static__control">
-                                    <Button text="Подписаться"  onAction={this.openModal} type={Button.types.link}/>
+                                    <Button text="Подписаться" onAction={this.openModal} type={Button.types.link}/>
                                 </div>
                                 <DonatForm author={login}/>
                             </div>
