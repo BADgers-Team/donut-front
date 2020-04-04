80827751cd343821feb1203f6b67732954383a21
kate <kirilek@bk.ru>
2020-04-04 14:05:44 +0300

    added likes and views to post

--- src/components/blocks/block-post-static/block-post-static.js
+++ src/components/blocks/block-post-static/block-post-static.js
@@ -8,6 +8,8 @@ import SubscriptionIcon from 'assets/img/subscription.svg';
 import CalendarIcon from 'assets/img/calendar.svg';
 import Button from 'components/fragments/button/button';
 import BlockPayment from 'components/blocks/block-payment/block-payment';
+import Like from 'components/blocks/block-like/block-like';
+import Seen from 'components/blocks/block-seen/block-seen';
 import { DonatForm } from 'components/blocks/block-post-static/donat-form/donat-form';
 import { PRIVACY } from 'store/const';
 import RouteStore from 'store/routes';
@@ -36,6 +38,10 @@ class BlockPostStatic extends Component {
         const profileRoute = getRouteWithID(RouteStore.pages.user.profile, login);
         const visibility = post.visible_type === 'Открыт для всех' ? 'Это публичный пост :)' : post.visible_type;
         const date = post.date || '23 февраля 2019';
+        const postId = post.id;
+        const likes = post.likes_count || 0;
+        const currentUserLiked = post.liked;
+        const seen = post.views_count || 1;
         return (
             <>
                 <BlockPayment 
@@ -63,6 +69,15 @@ class BlockPostStatic extends Component {
                         <div className="post-static__info">
                             <img className="post-static__info-icon" src={SubscriptionIcon} alt="subscription"/>
                             <div>{visibility}</div>
+                        </div>
+                        <div className="post-static__info">
+                            <Like likesCount={likes} currentUserLiked={currentUserLiked} postId={postId}
+                            likedClass="post-static__info__icon" 
+                            dislikedClass="post-static__info__icon post-static__info-disliked__icon" 
+                            textClass="post-static__info__text"/>
+                            <Seen seen={seen} 
+                            iconClass="post-static__info__icon" 
+                            textClass="post-static__info__text"/>
                         </div>
 
                         {post.visible_type === PRIVACY.OPEN && (
