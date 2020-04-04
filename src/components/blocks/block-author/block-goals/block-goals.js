ee0633c607021a9691602e29f97ce143591a6b6d
Erzhebet <dobrl2011@mail.ru>
2020-04-04 16:23:01 +0300

    fix

--- src/components/blocks/block-author/block-goals/block-goals.js
+++ src/components/blocks/block-author/block-goals/block-goals.js
@@ -7,7 +7,7 @@ class BlockGoals extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isModalOpen: true,
+            isModalOpen: false,
             goals: [],
         };
     }
