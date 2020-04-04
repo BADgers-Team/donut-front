58083dda0e36be22b6e93fe11ec95be30c7c3a27
Erzhebet <dobrl2011@mail.ru>
2020-04-04 15:31:19 +0300

    Added styles

--- src/components/blocks/block-author/block-goals/goal-modal/goal-modal.js
+++ src/components/blocks/block-author/block-goals/goal-modal/goal-modal.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BlockModal } from 'components/blocks/block-modal/block-modal';
 import Button from 'components/fragments/button/button';
+import RouteStore from 'store/routes';
+import AjaxModule from 'services/ajax';
+
+import './goal-modal.scss';
 
 const MODAL_TITLE = 'Новая цель';
 
@@ -25,9 +29,21 @@ export class GoalModal extends Component {
         this.setState({ sum: value });
     };
 
-    handleSumbit = (event) => {
+    handleSubmit = (event) => {
+        const { goal, sum } = this.state;
+        const { onSuccess } = this.props;
         event.preventDefault();
         const form = event.target;
+        if (form) {
+            AjaxModule.post(RouteStore.api.goals, {
+                title: goal,
+                sum_wanted: +sum,
+            }).then((data) => {
+                onSuccess && onSuccess(data);
+            }).catch((error) => {
+                console.log(error.message);
+            });
+        }
     };
 
     render() {
@@ -40,13 +56,16 @@ export class GoalModal extends Component {
                 title={MODAL_TITLE}
                 onClose={onClose}
             >
-                <form onSubmit={this.handleSumbit}>
-                    <div>Расскажите своим подписчикам, чего вы хотите достичь с помощью Give me a donut!</div>
-                    <label htmlFor="goal-title">Моя цель</label>
-                    <input type="text" name="goal-title" value={goal} placeholder="Опишите кратко свою цель" onChange={this.handleGoalChange}/>
-                    <label htmlFor="goal-sum">Я хочу собрать</label>
-                    <input type="number" min="16" max="2147483647" name="goal-sum" value={sum} onChange={this.handleSumChange}/>
-                    <Button type={Button.types.submit} value="Добавить цель"/>
+                <form className="goal__form" onSubmit={this.handleSubmit}>
+                    <div className="goal__subtitle">Расскажите своим подписчикам, чего вы хотите достичь с помощью Give me a donut!</div>
+                    <label className="goal__label" htmlFor="title">Моя цель</label>
+                    <textarea className="goal__textarea" name="title" value={goal} placeholder="Опишите кратко свою цель" onChange={this.handleGoalChange}/>
+                    <label className="goal__label" htmlFor="sum">Я хочу собрать</label>
+                    <div>
+                        <input className="goal__input" type="number" min="16" max="2147483647" name="sum" value={sum} onChange={this.handleSumChange}/>
+                        <label className="goal__input-after">₽</label>
+                    </div>
+                    <Button className="goal__submit" type={Button.types.submit} text="Добавить цель"/>
                 </form>
             </BlockModal>
         );
